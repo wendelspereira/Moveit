99e0323e156e4944c5c0c7294378d1403b25696f
Wendel S. Pereira <wendelspereira@outlook.com>
2021-02-25 19:02:24 -0300

    implementando o ChallengeBox

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox() {
+
+    const hasActiveChallenge = true
+
+    return (
+        <div className={styles.challengeBoxContainer}>
+            <div className={styles.challengeBoxNotActive}>
+                <strong>Finalize uma cliclo para receber um desafio</strong>
+                <p>
+                    <img src="icons/level-up.svg" alt="Level Up" />
+                Avance de level completando desafios.
+            </p>
+            </div>
+        </div >
+    )
+}
+
