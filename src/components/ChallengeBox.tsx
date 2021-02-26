bb9d390d5d370363f69dd2f9227bc3d2ed060807
Wendel S. Pereira <wendelspereira@outlook.com>
2021-02-26 16:27:02 -0300

    finalizada a interface gráfica

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -6,13 +6,40 @@ export function ChallengeBox() {
 
     return (
         <div className={styles.challengeBoxContainer}>
-            <div className={styles.challengeBoxNotActive}>
-                <strong>Finalize uma cliclo para receber um desafio</strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up" />
-                Avance de level completando desafios.
-            </p>
-            </div>
+
+            {hasActiveChallenge ? (
+                <div className={styles.challengeBoxActive}>
+                    <header>
+                        Ganhe 400 XP
+                    </header>
+                    
+                    <main>
+                        <p>
+                            <img src="icons/body.svg" alt="icon"/>
+                        </p>
+                            <strong>Novo desafio</strong>
+                            <p>Levante e faça uma caminhada de 3 minutos.</p>
+                    </main>
+
+                    <footer>
+                        <button type="button" className={styles.challengeFailedButton}>
+                            Falhei
+                        </button>
+
+                        <button type="button" className={styles.challengeSuccessButton}>
+                            Completei
+                        </button>
+                    </footer>
+                </div>
+            ) : (
+                    <div className={styles.challengeBoxNotActive}>
+                        <strong>Finalize uma cliclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level Up" />
+                            Avance de level completando desafios.
+                        </p>
+                    </div>
+                )}
         </div >
     )
 }
