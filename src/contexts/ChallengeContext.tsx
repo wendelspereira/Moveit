bd1f8bd233aaaf961ee2afb812c6ea0265715009
Wendel S. Pereira <wendelspereira@outlook.com>
2021-02-26 19:38:05 -0300

    implementado o contexto para comunicação entre os componentes e popupalada a 'ChallengeBox' com os dados do json por meio do context

---
+++ src/contexts/ChallengeContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState } from "react"
+import challenges from "../../challenges.json"
+
+interface challenge {
+    type: 'eye' | 'body';
+    description: string;
+    amount: number;
+}
+
+interface ChallengesContextData {
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+    levelUp: () => void;
+    startNewChallenge: () => void;
+    activeChallenge: challenge;
+    
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData)
+
+export function ChallengesProvider({children}) {
+const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
+    const [activeChallenge, setActiveChallenge] = useState(null)
+    
+    function levelUp(){
+        setLevel(level + 1)
+    }
+
+    function startNewChallenge() {
+        const randomChallegesIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[randomChallegesIndex]
+        setActiveChallenge(challenge)
+    }
+
+    return (
+        <ChallengesContext.Provider 
+        value={{ 
+            level, 
+            currentExperience,
+            challengesCompleted,
+            levelUp,
+            startNewChallenge,
+            activeChallenge
+        }}
+        >
+            {children}
+        </ChallengesContext.Provider>
+    )
+}
